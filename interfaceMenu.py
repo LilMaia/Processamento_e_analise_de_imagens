f2107e99ea400f447548970df0f59abe56a3cd12
chiucchi <gchiucchi01@gmail.com>
2023-05-08 22:20:29 -0300

    leitura dos arquivos e aumento do dataset de treino

--- interfaceMenu.py
+++ interfaceMenu.py
@@ -8,7 +8,7 @@ Jonathan Tavares - 540504
 Giulia Chiucchi - 662103
 """
 
-#Imports
+# Imports
 import tkinter as tk
 
 from imageInfo import ImageInfo
@@ -16,6 +16,8 @@ from imageUtils import abrir_imagem
 from zoom import resetar_zoom, aumentar_zoom
 from contraste import ajustar_contraste
 from segmentar import segmentar_mama
+from treino import generateTrainImages
+
 
 def interfaceMenu():
     # Cria o objeto que conterá as imagens
@@ -23,8 +25,9 @@ def interfaceMenu():
 
     # Cria a janela principal e define o título
     root = tk.Tk()
-    root.title("Trabalho de Processamento e Análise de Imagens - Ciência da Computação - 2023/1")
-    
+    root.title(
+        "Trabalho de Processamento e Análise de Imagens - Ciência da Computação - 2023/1")
+
     # Cria a janela secundaria e define o título
     janela = tk.Toplevel(root)
     janela.title("Nova Imagem")
@@ -32,7 +35,8 @@ def interfaceMenu():
     image_label.pack()
 
     # Define as dimensões da janela e sua posição no centro da tela
-    root.geometry(f"{600}x{300}+{int((root.winfo_screenwidth() / 2) - (600 / 2))}+{int((root.winfo_screenheight() / 2) - (300 / 2))}")
+    root.geometry(
+        f"{600}x{300}+{int((root.winfo_screenwidth() / 2) - (600 / 2))}+{int((root.winfo_screenheight() / 2) - (300 / 2))}")
     root.resizable(False, False)
 
     # Criando a barra de menus
@@ -45,28 +49,39 @@ def interfaceMenu():
     menu_bar.add_cascade(label="Opções", menu=file_menu)
 
     # Cria o botão "Abrir" e adiciona ele à janela principal
-    open_button = tk.Button(root, text="Abrir imagem", command=  lambda : abrir_imagem(image_label,image_info))
+    open_button = tk.Button(root, text="Abrir imagem",
+                            command=lambda: abrir_imagem(image_label, image_info))
     open_button.pack()
 
     # Cria o botão "Zoom In" e adiciona ele à janela principal
-    zoom_in_button = tk.Button(root, text="Aumentar Zoom", command= lambda: aumentar_zoom(image_label, image_info))
+    zoom_in_button = tk.Button(
+        root, text="Aumentar Zoom", command=lambda: aumentar_zoom(image_label, image_info))
     zoom_in_button.pack()
 
     # Cria o botão "Zoom Out" e adiciona ele à janela principal
-    zoom_out_button = tk.Button(root, text="Retornar a imagem original", command=lambda: resetar_zoom(image_label, image_info))
+    zoom_out_button = tk.Button(root, text="Retornar a imagem original",
+                                command=lambda: resetar_zoom(image_label, image_info))
     zoom_out_button.pack()
-    
+
     # Cria o slider para ajustar o contraste mínimo
-    min_value_slider = tk.Scale(root, from_=0, to=255, orient="horizontal", label="Aum. Contraste", command= lambda val: ajustar_contraste(min_value_slider.get(), max_value_slider.get(), image_label, image_info))
+    min_value_slider = tk.Scale(root, from_=0, to=255, orient="horizontal", label="Aum. Contraste", command=lambda val: ajustar_contraste(
+        min_value_slider.get(), max_value_slider.get(), image_label, image_info))
     min_value_slider.pack()
 
     # Cria o slider para ajustar o contraste máximo
-    max_value_slider = tk.Scale(root, from_=0, to=255, orient="horizontal", label="Dim. Contraste", command= lambda val: ajustar_contraste(min_value_slider.get(), max_value_slider.get(), image_label, image_info))
+    max_value_slider = tk.Scale(root, from_=0, to=255, orient="horizontal", label="Dim. Contraste", command=lambda val: ajustar_contraste(
+        min_value_slider.get(), max_value_slider.get(), image_label, image_info))
     max_value_slider.pack()
-    
+
     # Cria o botão "Segmentar" e adiciona ele à janela principal
-    segmentar_button = tk.Button(root, text="Segmentar", command= lambda: segmentar_mama(image_info, image_label))
+    segmentar_button = tk.Button(
+        root, text="Segmentar", command=lambda: segmentar_mama(image_info, image_label))
     segmentar_button.pack()
+
+    # Cria o botão de treino e o adiciona à janela principal
+    generate_images_button = tk.Button(
+        root, text="Gerar imagens de treinio", command=lambda: generateTrainImages())
+    generate_images_button.pack()
 
     # Inicia o loop principal da janela
     root.mainloop()
